329fcc4af3e5d9f2210a0808b0cdac97597e7477
INewWorldI <newworld-@naver.com>
2023-03-15 12:13:15 +0900

    프로젝트 업로드

---
+++ server/src/main/java/dev/nworld/nendoro/NendoroApplication.java
@@ -0,0 +1,14 @@
+package dev.nworld.nendoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NendoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NendoroApplication.class, args);
+	}
+
+}
+s
---
+++ server/src/test/java/dev/nworld/nendoro/NendoroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nworld.nendoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NendoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
