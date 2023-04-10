a636d269108a923d3bfef6d762e987f0d5b09eec
INewWorldI <newworld-@naver.com>
2023-04-10 23:29:37 +0900

    fix: 일부 수정 문제 해결

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const getData = async () => {
-      const res = await fetch(
-        'http://localhost:8090/hello=Spring'
-      ).then((res) => res.json());
-      console.log(res); // 500 개의 데이터 확인 가능
-    };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
